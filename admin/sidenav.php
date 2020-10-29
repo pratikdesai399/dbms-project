d35387dc81f38d15f8c50b460852f8da60494e77
Pratik Desai <pratikdesai399@gmail.com>
2020-10-30 00:52:59 +0530

    Admin Completed

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -7,6 +7,7 @@
 
 	<div class="list-group bg-info" style="height: 90vh;">
 		<a href="index.php" class="list-group-item list-group-item-action bg-info text-center text-white">DASHBOARD</a>
+		<a href="profile.php" class="list-group-item list-group-item-action bg-info text-center text-white">PROFILE</a>
 
 		<a href="admin.php" class="list-group-item list-group-item-action bg-info text-center text-white">ADMINS</a>
 
